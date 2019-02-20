0c7892fa09f5b77f4f84cc23d1989c599bd74054
Ellen Sun <heeycutie@yahoo.com>
2019-02-19 20:51:36 -0500

    DELETE comments

--- scraper.py
+++ scraper.py
@@ -32,28 +32,6 @@ class MovieDb():
             for i in range(self.limit):
                 print(data[i])
 
-    # i = 0
-    # if user_choice == '1':
-
-        # url = "https://www.imdb.com/chart/top?ref_=nv_mv_250"
-        # ourUrl = urllib3.PoolManager().request('GET', url).data
-        # soup = BeautifulSoup(ourUrl, 'html.parser')
-        # links = soup.find_all("td", class_="titleColumn")
-    #     for i in range(limit):
-    #         movie_info = re.sub('\W+',' ', links[i].text)
-    #         print(movie_info)
-    # elif user_choice == '2':
-        # Using requests with xpath and lxml:
-        # url2 = "https://www.imdb.com/best-of/top-100-shows-of-2018/ls045252633/?pf_rd_m=A2FGELUUNOQJNL&pf_rd_p=b01789be-dffd-4aff-a31d-6eead051021a&pf_rd_r=GR0XJ2Y6DMB1YVXBSKAA&pf_rd_s=center-7&pf_rd_t=60601&pf_rd_i=best-of&ref_=fea_bo16_pks_tv_hd"
-        # imdb_page = requests.get(url2)
-        # # page_content = BeautifulSoup(imdb_page.content, "html.parser")
-        # page_content = html.fromstring(imdb_page.content) # need
-        # movie_block = page_content.xpath('//*[@data-caller-name="list-title"]/div/div/div/h3[@class="lister-item-header"]/a/text()')
-    #     for i in range(limit):
-    #         print(movie_block[i])
-    # else:
-    #     print("Not a valid choice! Choose again!")
-
 if __name__ == "__main__":
     print("I want to see top movies:\n1. ALL \n2. 2018")
     user_choice = input("Choice (1, 2):")
