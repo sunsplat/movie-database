0223108e95a529c2bd7e870dfe6c33b0ca893ec6
Ellen Sun <heeycutie@yahoo.com>
2019-02-19 19:55:41 -0500

    ADD requests call and lxml, ADD README.md

---
+++ Readme.md
@@ -0,0 +1,15 @@
+Top Movies
+------------------
+Python script using Beautiful Soup
+
+## To Run Locally:
+Clone repo   
+Install dependencies:
+`pip3 install -r requirements.txt`     
+`python scraper.py`
+
+------------------
+Resources:
+[BeautifulSoup](https://www.crummy.com/software/BeautifulSoup/bs4/doc/)
+[find() and find_all()](https://lucaseo.github.io/2018/02/19/reviewNote-20180219-2/)
+[lxml](https://lxml.de/)
--- scraper.py
+++ scraper.py
@@ -1,14 +1,26 @@
-import urllib3
+import requests, urllib3
+from lxml import html, etree
 from bs4 import BeautifulSoup
 
+# Using urllib3 with BeautifulSoup:
 url = "https://www.imdb.com/chart/top?ref_=nv_mv_250"
 ourUrl = urllib3.PoolManager().request('GET', url).data
 soup = BeautifulSoup(ourUrl, 'html.parser')
 links = soup.select("table.chart .lister-list a")
-# print(links)
-for link in links:
-    # link = link.replace('\n', ' ').replace('\r', '')
-    print(link)
+
+# Using requests with xpath and lxml:
+url2 = "https://www.imdb.com/best-of/top-100-shows-of-2018/ls045252633/?pf_rd_m=A2FGELUUNOQJNL&pf_rd_p=b01789be-dffd-4aff-a31d-6eead051021a&pf_rd_r=GR0XJ2Y6DMB1YVXBSKAA&pf_rd_s=center-7&pf_rd_t=60601&pf_rd_i=best-of&ref_=fea_bo16_pks_tv_hd"
+imdb_page = requests.get(url2)
+page_content = html.fromstring(imdb_page.content) # need
+movie_block = page_content.xpath('//*[@data-caller-name="list-title"]/div/div/div/h3[@class="lister-item-header"]/a/text()')
+for block in movie_block:
+    print(block)
+    # print(etree.tostring(block))
+
+
+# links2 = soup.findAll('h3', attrs={'class':'image'})
+# for link in links:
+#     print(link)
 
 # print(soup.find('title').text)
 # print(soup.title.string)
